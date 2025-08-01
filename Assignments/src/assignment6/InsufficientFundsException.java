63a6b70b71c2e80db52159ac2794beb63a37fac5
HP <HP@DESKTOP-FO9L3V5.mshome.net>
2025-08-01 12:12:30 +0530

    This sample program for exception handling in java

---
+++ Assignments/src/assignment6/InsufficientFundsException.java
@@ -0,0 +1,10 @@
+package assignment6;
+
+
+	public class InsufficientFundsException extends Exception {
+	    public InsufficientFundsException(String message) {
+	        super(message);
+	    }
+	
+
+}
